b8756288d200e7d9c6c32528c36ac1bfb65ac3aa
VIKHYAT SINGH <166906251+Vikhyat-Singh12@users.noreply.github.com>
2025-06-24 00:20:44 +0530

    Update test_mongo_dp.py

--- test_mongo_dp.py
+++ test_mongo_dp.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://coc9mzp:BMljZnrGsbpclhHL@security.ju52ihr.mongodb.net/?retryWrites=true&w=majority&appName=Security"
+uri = "XYZ"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
